c0f53ec1e943c2d9dccddc9cee1e64519871ea38
LUIS FERNANDO LIRA COELHO <fernandolirac2005@gmail.com>
2025-12-19 19:49:13 -0300

    erro no supabase

--- supabase.js
+++ supabase.js
@@ -1,5 +1,8 @@
-<script src="https://unpkg.com/@supabase/supabase-js@2"></script>
-const supabase = supabase.createClient(
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabase = createClient(
     "https://ttdyyvslxuugvleqyeep.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0ZHl5dnNseHV1Z3ZsZXF5ZWVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNzQ2MzcsImV4cCI6MjA4MTc1MDYzN30.HDMVN3667UhB6M3w-3apemqQBd6r-kmw4jENn6YF_HM"
 );
+
+window.supabase = supabase;
